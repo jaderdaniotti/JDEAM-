7390b86ddc37a585ca399afa9007ef83acc6a46d
jader <jaderdaniotti1999@gmail.com>
2025-03-24 14:56:02 +0100

    navbar done

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,75 @@
+<nav class="navbar navbar-expand-lg bg-navbar">
+    <div class="container-fluid px-5">
+        <div class="d-flex p-1 justify-content-center align-items-center">
+            <a class="nav-link active" aria-current="page" href="#">
+                <img src="/images/LOGO/fullstack.png" alt="Logo" height="100">
+            </a>
+            <a class="nav-link active darker-grotesque text-dark" href="#">Jdeam</a>
+
+        </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav  mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active agdasima-regular" href="#">NEGOZIO</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active agdasima-regular" href="#">COMUNITà</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active agdasima-regular" href="#">Informazioni</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active agdasima-regular" href="#">Assistenza</a>
+              </li>
+        </ul>
+        
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Cerca un prodotto..." aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Cerca</button>
+        </form>
+        <div class="d-flex mx-2  justify-content-center">
+            <a href="" class="btn"><i class="bi bi-person-add"></i></a>
+            <a href="" class="btn mx-1"><i class="bi bi-box-arrow-in-right"></i></a>
+          </div>
+      </div>
+    </div>
+  </nav>
+{{-- <nav class="navbar navbar-expand-lg bg-navbar">
+  <div class="container-fluid">
+    <!-- Toggler per schermi piccoli -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon color-white"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <!-- Navbar per schermi grandi e medi (sopra lg) -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">
+            <img src="/images/LOGO/fullstack.png" alt="Logo" height="100">
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active darker-grotesque text-white" href="#">Jdeam</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active agdasima-regular" href="#">NEGOZIO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active agdasima-regular" href="#">COMUNITà</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active agdasima-regular" href="#">Informazioni</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active agdasima-regular" href="#">Assistenza</a>
+        </li>
+      </ul>
+
+      
+    </div>
+  </div>
+</nav> --}}
